a377d852baeda09e78479da189a05e880c15614c
x719554636 <719554636@qq.com>
2019-10-04 13:07:27 +0800

    第一次提交

--- application/ebapi/controller/AuthController.php
+++ application/ebapi/controller/AuthController.php
@@ -15,4 +15,16 @@ class AuthController extends Basic
         $this->userInfo=$this->checkTokenGetUserInfo();
         $this->uid=isset($this->userInfo['uid']) ? $this->userInfo['uid'] : 0;
     }
+    /**
+     * 统一返回格式
+     * @param array $data
+     * @param string $code
+     * @param string $msg
+     * @return array
+     */
+    public static function asJson($data = [],$code = '000000',$msg = '')
+    {
+        return json_encode(['data' => $data,'code' => (string)$code,'msg' => $msg]);
+    }
+
 }
