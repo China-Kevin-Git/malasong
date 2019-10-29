4b8c0d4c6e634194018a91f0cd349644c3db7945
test <719554636@qq.com>
2019-10-29 11:25:55 +0800

    图

--- application/ebapi/controller/MatchBargains.php
+++ application/ebapi/controller/MatchBargains.php
@@ -29,7 +29,7 @@ class MatchBargains extends AuthController
      */
     public function get_bargain_config(){
         $info = [
-            "http://chinb.org/system/images/20191012003902.png"
+            "http://chinb.org/public/system/images/20191012003902.png"
         ];
         return JsonService::successful($info);
     }
