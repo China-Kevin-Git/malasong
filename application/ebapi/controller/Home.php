55ddbd61dd64007d56bb8576b98150e1b9b12ac8
test <719554636@qq.com>
2019-10-29 17:34:20 +0800

    赛事三级联动

--- application/ebapi/controller/Home.php
+++ application/ebapi/controller/Home.php
@@ -26,9 +26,9 @@ class Home extends AuthController
         }
         $data["icon"] = Db::name("store_category")->field("id,cate_name,pic")->order("sort")->select();
 
-        $data["combination"] = Db::name("store_combination")->field("image,product_id as id,price,title")->where(['is_del'=>0,"is_show"=>1])->where("stop_time",'>',time())->order("sort")->select();
-        $data["seckill"] = Db::name("store_seckill")->field("image,stop_time,product_id as id,price,title,ot_price")->where(['is_del'=>0,"is_show"=>1])->where("stop_time",'>',time())->order("sort")->select();
-        $data["bargain"] = Db::name("store_bargain")->field("image,product_id as id, '0元购' as `price_name`")->where(['is_del'=>0])->where("stop_time",'>',time())->order("sort")->select();
+        $data["combination"] = Db::name("store_combination")->field("id,image,product_id,price,title")->where(['is_del'=>0,"is_show"=>1])->where("stop_time",'>',time())->order("sort")->select();
+        $data["seckill"] = Db::name("store_seckill")->field("id,image,stop_time,product_id,price,title,ot_price")->where(['is_del'=>0,"is_show"=>1])->where("stop_time",'>',time())->order("sort")->select();
+        $data["bargain"] = Db::name("store_bargain")->field("id,image,product_id, '0元购' as `price_name`")->where(['is_del'=>0])->where("stop_time",'>',time())->order("sort")->select();
 
         return self::asJson($data);
     }
