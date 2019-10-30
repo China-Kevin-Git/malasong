270f4f66dd9f71de91ef8983d68d6360c929594d
test <719554636@qq.com>
2019-10-30 11:28:45 +0800

    1

--- application/database.php
+++ application/database.php
@@ -12,23 +12,23 @@
 return [
     // 数据库类型
     'type'            => 'mysql',
-//    // 服务器地址
-//    'hostname'        => '127.0.0.1',
-//    // 数据库名
-//    'database'        => 'crmeb',
-//    // 用户名
-//    'username'        => 'crmeb',
-//    // 密码
-//    'password'        => '123456',
-
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'crmeb',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'crmeb',
     // 密码
-    'password'        => 'root',
+    'password'        => '123456',
+
+    // 服务器地址
+//    'hostname'        => '127.0.0.1',
+//    // 数据库名
+//    'database'        => 'crmeb',
+//    // 用户名
+//    'username'        => 'root',
+//    // 密码
+//    'password'        => 'root',
 //
 //    // 端口
 //    'hostport'        => '3306',
