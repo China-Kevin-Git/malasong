8fe3e14c7fb821db70515cc8dd0c38e0bbb87adb
test <719554636@qq.com>
2019-10-30 12:41:41 +0800

    1

--- application/ebapi/controller/Macth.php
+++ application/ebapi/controller/Macth.php
@@ -337,7 +337,6 @@ class Macth extends AuthController
         }
         $order_price=$pricee+$meal_price+$match_goods_price;
         $order_price = round($order_price,2);
-        $order_price=0.01;
 
         $add=[
             "uid"=>$this->uid,
